fa0ad56e0141cd49e26e04e0a104fa5046150e7b
cucumber99 <dlsguq1@gmail.com>
2025-03-03 22:08:45 +0900

    feat: 댓글 작성 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.getElementById("submitComment").addEventListener("click", function () {
+    const commentInput = document.getElementById("commentInput");
+    const commentText = commentInput.value.trim();
 
-console.log("알맞은 스크립트를 작성하세요");
+    if (commentText === "") {
+        alert("댓글을 입력해주세요.");
+        return;
+    }
+
+    const newComment = document.createElement("li");
+    newComment.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">${commentText}</div>
+      </div>
+    `;
+
+    document.getElementById("commentList").appendChild(newComment);
+
+    commentInput.value = "";
+
+    alert("댓글이 등록되었습니다.");
+})
